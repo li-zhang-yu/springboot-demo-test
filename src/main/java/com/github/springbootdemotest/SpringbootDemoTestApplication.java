0de8254d4198136b6bd084be268d80680dcf9b68
lzy8339789 <1641189550@qq.com>
2019-06-09 11:54:04 +0800

    Initial commit

---
+++ src/main/java/com/github/springbootdemotest/SpringbootDemoTestApplication.java
@@ -0,0 +1,13 @@
+package com.github.springbootdemotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoTestApplication.class, args);
+    }
+
+}
