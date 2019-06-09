d89bb5572ea5d226eb17eed7f4f2c5c2ce7e228b
lzy8339789 <1641189550@qq.com>
2019-06-09 16:09:05 +0800

    测试

---
+++ src/main/java/com/github/springbootdemotest/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.github.springbootdemotest.controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/demo")
+    public String test() {
+        System.out.println(123);
+        return "123";
+    }
+
+}
