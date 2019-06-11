fb585ad8894ad285db8afdfb302aef60ad16a4f7
lzy8339789 <1641189550@qq.com>
2019-06-11 22:14:01 +0800

    测试

--- src/main/shell/test.sh
+++ src/main/shell/test.sh
@@ -4,7 +4,10 @@
 #echo "123456"
 
 demoFun(){
- echo "123"
+ # 拉取镜像
+ sudo docker push registry.cn-hangzhou.aliyuncs.com/lizhangyu/springboot-demo-test:v2
+ #运行镜像并删除容器
+ sudo docker run -p 8080:8080 --rm=true -it registry.cn-hangzhou.aliyuncs.com/lizhangyu/springboot-demo-test:v2
 }
 
 demoFun
